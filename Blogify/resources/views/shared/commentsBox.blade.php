d96bfcf17562f36cb5d99fbbcbba1611dda4e487
nooreyad <nooreyadd39@gmail.com>
2024-08-06 06:34:39 +0300

    text truncation

--- Blogify/resources/views/shared/commentsBox.blade.php
+++ Blogify/resources/views/shared/commentsBox.blade.php
@@ -9,15 +9,17 @@
         </div>
     </form>
     @foreach ($blog->comments as $comment)
-        <div class="d-flex align-items-start mb-3 mt-2">
+        <div class="d-flex align-items-start mb-2 mt-2">
             <img style="width:35px" class="me-2 avatar-sm rounded-circle" src="{{ $comment->user->getImageURL() }}"
                 alt="{{ $comment->user->name }}">
-            <div class="w-100">
-                <div class="d-flex justify-content-between flex-wrap">
+            <div class="w-100 text-truncate">
+                <div class="d-flex justify-content-between">
                     <h6 class="mb-1">{{ $comment->user->name }}</h6>
                     <small class="fs-6 fw-light text-muted">{{ $comment->created_at->diffForHumans() }}</small>
                 </div>
-                <p class="fs-6 mt-2 fw-light mb-0 text-wrap">{{ $comment->content }}</p>
+                <div>
+                    <p class="fs-6 mt-1 fw-light mb-0 text-truncate">{{ $comment->content }}</p>
+                </div>
             </div>
         </div>
     @endforeach
